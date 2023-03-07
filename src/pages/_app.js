7f7bf757707c97a51d275771284231e6114e4b8e
Lum Bulliqi <1lumbulliqi@gmail.com>
2023-03-07 15:06:03 +0100

    merge next with static style

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,10 @@
-import '@/styles/globals.css'
+import "@/styles/animate.css";
+import "@/styles/bootstrap.min.css";
+import "@/styles/font-awesome.min.css";
+import "@/styles/linear-icons.css";
+import "@/styles/magnific-popup.css";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
