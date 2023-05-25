9db16f6562b9b768fb744672a5ee5d8608bf38c8
Lum Bulliqi <1lumbulliqi@gmail.com>
2023-05-25 14:05:24 +0200

    logo changed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,13 +6,27 @@ import "@/styles/linear-icons.css";
 import "@/styles/font-awesome.min.css";
 import { useEffect } from "react";
 import Router from "next/router";
+import Image from "next/image";
 
 function App({ Component, pageProps }) {
   useEffect(() => {
     Router.push("/maintenance"); // replace "/my-page" with the path of the page you want to redirect to
   }, []);
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <>
+        <Image
+          src="/banner.jpg"
+          style={{ objectFit: "contain" }}
+          unoptimized
+          priority
+          fill="false"
+          alt="Fletushka Online"
+        />
+      </>
+    </>
+  );
 }
 
 App.getInitialProps = async ({ Component, ctx }) => {
